a902673899bd7604bb0070e8778ac2f0b754765c
Blake <blake@sightsource.net>
2018-08-09 15:39:47 -0400

    deleted test

--- glacier_test.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');  
-
-file = "glacier_log/01Apr16.log"
-
-fs.readFile(file, 'utf16le', function(err, contents) {
-    console.log(JSON.parse(contents.substr(1))['archiveId']);
-   });
